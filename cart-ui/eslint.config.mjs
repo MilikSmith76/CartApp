140be293978fcb9da1eaeb0512347791acc497bc
MilikSmith76 <miliksmith23@gmail.com>
2026-01-18 14:43:25 -0500

    Configure Linter and Prettier

--- cart-ui/eslint.config.mjs
+++ cart-ui/eslint.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import perfectionist from 'eslint-plugin-perfectionist';
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
@@ -13,6 +14,24 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      'jsx-quotes': ['error', 'prefer-single'],
+      'perfectionist/sort-classes': 'error',
+      'perfectionist/sort-exports': 'error',
+      'perfectionist/sort-imports': 'error',
+      'perfectionist/sort-interfaces': 'error',
+      'perfectionist/sort-jsx-props': 'error',
+      'perfectionist/sort-named-exports': 'error',
+      'perfectionist/sort-named-imports': 'error',
+      'perfectionist/sort-objects': 'error',
+      'quotes': ['error', 'single'],
+    }
+  }
 ]);
 
 export default eslintConfig;
---
+++ cart-ui/prettier.config.mjs
@@ -0,0 +1,12 @@
+const config = {
+  bracketSameLine: false,
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  singleQuote: true,
+  tabWidth: 4,
+  trailingComma: 'es5',
+};
+
+export default config;
