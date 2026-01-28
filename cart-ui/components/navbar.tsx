88ee17692e690dc6ca772f64072cfa8f35315156
MilikSmith76 <miliksmith23@gmail.com>
2026-01-28 16:54:02 -0500

    Remove Link component usage in Navbar component

--- cart-ui/components/navbar.tsx
+++ cart-ui/components/navbar.tsx
@@ -28,13 +28,13 @@ const Navbar = (): JSX.Element => {
                 </div>
                 <div className='ml-10 hidden items-baseline space-x-4 md:flex'>
                     {links.map((link) => (
-                        <Link
+                        <a
                             className='text-md rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white'
                             href={link.href}
                             key={link.name}
                         >
                             {link.name}
-                        </Link>
+                        </a>
                     ))}
                     <div className='p-4 px-3 py-2' />
                 </div>
